61690e8471da8ab6f5a0ae75a9103e4589f6d7bb
llauu <lautaromonserrat13@gmail.com>
2022-09-13 16:47:41 -0300

    first

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,16 @@
+/*
+Trabajo Practico 1
+
+Lautaro Monserrat
+45426439
+DIV C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello World!\n");
+
+	return EXIT_SUCCESS;
+}
